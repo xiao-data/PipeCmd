778c8d7483b0747e3fb37a06158fbbfca2a46a91
xiao-data <1065377855@qq.com>
2020-10-21 21:28:49 +0800

    add z_strcmp

--- PipeCmd/fun.h
+++ PipeCmd/fun.h
@@ -5,6 +5,7 @@
 #define z_ZeroMemory(Destination,Length) z_memset((Destination),0,(Length))
 
 int z_strncmp(const char* str1, const char* str2, DWORD n);
+int z_strcmp(const char* str1, const char* str2);
 void z_memset(void* Dst, int Val, DWORD n);
 DWORD z_strlen(const char* str);
 void z_strcpy(char* Des, char const* Src);
